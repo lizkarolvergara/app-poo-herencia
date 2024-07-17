71ad1ff6646b9fd5292e88eb93d81fed0c11bd5f
Liz Karol Vergara <cva.k16@gmail.com>
2024-07-16 20:39:14 -0500

    ejercicio herencia

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from model import Deportivo
+from model.Deportivo import Deportivo
+from model.Furgoneta import Furgoneta
 
 objDeportivo = Deportivo("Ferrari", "488", 2021, 330)
 print(objDeportivo.descripcion())
